e2136d0e1030ae2ce39f83a1f27caf799daf7b6e
bubbabuford <bajobozic@gmail.com>
2017-05-07 17:09:17 +0200

    first commit

---
+++ app/src/main/java/com/example/bajob/panimageview/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.bajob.panimageview;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.diegocarloslima.byakugallery.lib.TouchImageView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private TouchImageView touchImageView;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
